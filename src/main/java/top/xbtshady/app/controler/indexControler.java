2049747225e54f90731bd5871559d4dc566a1d18
xbtshady <943217258@qq.com>
2019-01-30 15:11:47 +0800

    jar包 2019/01/30

--- src/main/java/top/xbtshady/app/controler/indexControler.java
+++ src/main/java/top/xbtshady/app/controler/indexControler.java
@@ -1,7 +1,7 @@
 package top.xbtshady.app.controler;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class indexControler {
